7ad69316804507ad039c77e33defc51bae2cad25
mikeorgill <org21001@byui.edu>
2023-04-22 12:21:15 -0600

    Finished portal page design

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,15 @@
 const month=  ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 const date = new Date();
-const currentDay = date.getDay();
+const currentDay = date.getDate();
 const currentMonth = month[date.getMonth()];
 const year = date.getFullYear();
 const currentDate = `Current Date: ${currentMonth} 
 					${currentDay}, ${year}`;
-document.querySelector("#currentdate").textContent = currentDate;
-const getTemperature = '89';
-const getTemp = `Current Temperature: ${getTemperature}`;
-document.querySelector("#temp").textContent = getTemp;
+const copyRightYear = `${year}`;
+const dateMod = document.lastModified;
 
+
+document.querySelector("#currentdate").textContent = currentDate;
+document.querySelector("#year").textContent = copyRightYear;
+
+document.querySelector("#updatedLast").innerHTML = dateMod;
