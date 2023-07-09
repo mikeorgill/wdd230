7ed71e883c8ae92dd5fec6a03cc9a705f29b815f
mikeorgill <org21001@byui.edu>
2023-07-08 22:02:48 -0600

    Loading Start files for Project

---
+++ bountiful-foods/scripts/last-modified.js
@@ -0,0 +1,20 @@
+/* Last Date Modifed Footer*/
+
+/*----Get the Current Date----*/
+const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const date = new Date();
+const curDay = date.getDate();
+const curMonth =  month[date.getMonth()];
+const curYear = date.getFullYear();
+const curDate = `Current Date ${curMonth} ${curDay}, ${curYear}`;
+//console.log(curDate);
+document.querySelector('#currentDate').innerHTML = curDate;
+
+/*----Get the Modified date----*/
+const mod = new Date(document.lastModified);
+const modDay = mod.getDate();
+const modMonth = month[mod.getMonth()];
+const modYear = mod.getFullYear();
+const modDate = `Last Updated ${modMonth} ${modDay}, ${modYear}`;
+// console.log(modDate);
+document.querySelector('#lastUpdated').innerHTML = modDate;
